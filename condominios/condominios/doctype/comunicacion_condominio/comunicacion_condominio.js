09f5a257fcaab88895994595708cc2a9ded46f2a
Ipasoft-sv <info@ipasoft.biz>
2020-08-03 02:07:38 +0200

    Primer doctype

---
+++ condominios/condominios/doctype/comunicacion_condominio/comunicacion_condominio.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Energenys-Ipasoft and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Comunicacion condominio', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ condominios/condominios/doctype/comunicacion_condominio/comunicacion_condominio.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Energenys-Ipasoft and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class Comunicacioncondominio(Document):
+	pass
