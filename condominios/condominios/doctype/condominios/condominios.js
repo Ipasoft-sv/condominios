8a764d68812f712cc953c36da8508054c5637de5
Ipasoft-sv <info@ipasoft.biz>
2020-08-03 02:19:44 +0200

    Renombrado doctype

---
+++ condominios/condominios/doctype/condominios/condominios.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Energenys-Ipasoft and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Condominios', {
+	// refresh: function(frm) {
+
+	// }
+});
